baa876ef69f47ef69a849be070e5384c341a2796
sutter-dave <sutter@intransix.com>
2021-07-19 18:19:05 -0700

    changes for the component CLASS_CONFIG update

--- src/SqliteQueryComponent.js
+++ src/SqliteQueryComponent.js
@@ -4,9 +4,11 @@ let {Component,getFormComponentDefaultMemberJson} = apogeeapp;
 /** This is a simple custom component example. */
 class SqliteQueryCell extends Component {};
 
-SqliteQueryCell.displayName = "SQLite Query Cell";
-SqliteQueryCell.uniqueName = "apogeeapp.SqliteQueryCell";
-SqliteQueryCell.DEFAULT_MEMBER_JSON = getFormComponentDefaultMemberJson("apogee.SqliteQueryMember");
+SqliteQueryCell.CLASS_CONFIG = {
+    displayName: "SQLite Query Cell",
+    uniqueName: "apogeeapp.SqliteQueryCell",
+    defaultMemberJson: getFormComponentDefaultMemberJson("apogee.SqliteQueryMember")
+}
 
 module.exports = SqliteQueryCell;
 
